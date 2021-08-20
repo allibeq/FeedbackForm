ec3337356d061da2574915e38c8701911e82855c
alina <69251156+allibeq@users.noreply.github.com>
2021-08-20 17:25:37 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -1,58 +1,80 @@
-'use strict'
+"use strict";
+const modalWindow = document.getElementById("modalWindow");
 
-document.addEventListener('DOMContentLoaded', function() {
-    
-    const email = document.querySelector('.emailInput');
-    const phone = document.querySelector('.phoneInput');
-    const form = document.getElementById('form');
-    form.addEventListener('submit', sendForm);
+const open = document
+  .getElementById("openWindow")
+  .addEventListener("click", () => {
+    modalWindow.classList.add("show");
+  });
+const close = document
+  .getElementById("closeButton")
+  .addEventListener("click", () => {
+    modalWindow.classList.remove("show");
+  });
+document.addEventListener("DOMContentLoaded", function () {
+  const email = document.querySelector(".emailInput");
+  const phone = document.querySelector(".phoneInput");
+  const form = document.getElementById("form");
+  form.addEventListener("submit", sendForm);
 
-    //запрет на стандартную отправку формы 
-    async function sendForm(e) {
-        e.preventDefault();
+  async function sendForm(e) {
+    e.preventDefault();
 
-        let error = validateInput(form);
+    let error = validateInput(form);
+  }
 
+  function validateInput(form) {
+    let formReq = document.querySelectorAll(".requiredElem");
+
+    formReq.forEach((element) => {
+      addClassError(element);
+    });
+
+    let name = formReq[0];
+    let email = formReq[1];
+    let phone = formReq[2];
+    let comment = formReq[3];
+
+    for (let id = 0; id < formReq.length; id++) {
+      const element = formReq[id];
+
+      if (element.id === "email" && element.value !== "") {
+        if (emailValidate(element)) {
+          removeClassError(element);
+
+          if (phoneValidate(phone)) {
+            removeClassError(phone);
+          } else if (phone.value === "") {
+            removeClassError(phone);
+          }
+        }
+      } else if (element.id === "phone" && element.value !== "") {
+        if (phoneValidate(element)) {
+          removeClassError(element);
+          if (emailValidate(email)) {
+            removeClassError(email);
+          } else if (email.value === "") {
+            removeClassError(email);
+          }
+        }
+      } else if (element.value !== "") {
+        removeClassError(element);
+      }
     }
+  }
+});
 
-    function validateInput(form){
-        let formReq = document.querySelectorAll('.requiredElem');
-        let error = 0;
-
-         for(let id = 0; id < formReq.length; id++){
-             const elements = formReq[id];
-             removeClassError(elements);
-
-             if(email.value.length > 0 || phone.value.length > 0) {
-                if(emailValidate()){
-                    phone.classList.remove('requiredElem')
-                } else if(phoneValidate()) {
-                    email.classList.remove('requiredElem')
-                } 
-                } else {
-                    if(elements.value === "") {
-                    addClassError(elements);
-                    error++;
-                    }else if(!emailValidate && !phoneValidate){
-                        alert('Проверьте правильность ввода полей!')
-                            addClassError(elements);
-                            error++;
-                        }
-             }
-
-         }
-    }
-
-    function addClassError(elements) {
-       elements.classList.add('errorClass');
-    }
-    function removeClassError(elements) {
-       elements.classList.remove('errorClass');
-    }
-    function emailValidate(){
-        return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(email.value);
-    }
-    function phoneValidate(){
-        return /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/.test(phone.value);
-    }
-})
+function addClassError(elements) {
+  elements.classList.add("errorClass");
+}
+function removeClassError(elements) {
+  elements.classList.remove("errorClass");
+}
+function emailValidate(email) {
+  return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(email.value);
+}
+function phoneValidate(phone) {
+  return /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/.test(
+    phone.value
+  );
+}
