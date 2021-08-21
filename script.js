e7d445a096d79983c610f902eddc386c462bef74
alina <69251156+allibeq@users.noreply.github.com>
2021-08-22 00:52:31 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -1,80 +1,163 @@
 "use strict";
-const modalWindow = document.getElementById("modalWindow");
+// для того чтобы функция отправки POST запроса работала, открывать страницу локально без хоста
+const email = document.querySelector(".emailInput");
+const phone = document.querySelector(".phoneInput");
+const url = "https://jsonplaceholder.typicode.com/users";
 
-const open = document
-  .getElementById("openWindow")
-  .addEventListener("click", () => {
-    modalWindow.classList.add("show");
-  });
-const close = document
-  .getElementById("closeButton")
-  .addEventListener("click", () => {
-    modalWindow.classList.remove("show");
-  });
 document.addEventListener("DOMContentLoaded", function () {
-  const email = document.querySelector(".emailInput");
-  const phone = document.querySelector(".phoneInput");
   const form = document.getElementById("form");
   form.addEventListener("submit", sendForm);
 
   async function sendForm(e) {
     e.preventDefault();
 
-    let error = validateInput(form);
-  }
+    let passes = validateInput(form);
+    if (passes === 4) {
+      const personsName = document.getElementById("name").value;
+      const phoneNumber = phone.value;
+      const emailAdress = email.value;
+      const message = document.getElementById("message").value;
 
-  function validateInput(form) {
-    let formReq = document.querySelectorAll(".requiredElem");
+      const body = {
+        name: personsName,
+        phoneNumber: phoneNumber,
+        emailAdress: emailAdress,
+        message: message,
+      };
 
-    formReq.forEach((element) => {
-      addClassError(element);
-    });
+      sendRequest("POST", url, body).catch((err) => console.log(err));
 
-    let name = formReq[0];
-    let email = formReq[1];
-    let phone = formReq[2];
-    let comment = formReq[3];
-
-    for (let id = 0; id < formReq.length; id++) {
-      const element = formReq[id];
-
-      if (element.id === "email" && element.value !== "") {
-        if (emailValidate(element)) {
-          removeClassError(element);
-
-          if (phoneValidate(phone)) {
-            removeClassError(phone);
-          } else if (phone.value === "") {
-            removeClassError(phone);
-          }
-        }
-      } else if (element.id === "phone" && element.value !== "") {
-        if (phoneValidate(element)) {
-          removeClassError(element);
-          if (emailValidate(email)) {
-            removeClassError(email);
-          } else if (email.value === "") {
-            removeClassError(email);
-          }
-        }
-      } else if (element.value !== "") {
-        removeClassError(element);
-      }
+      form.reset();
     }
   }
 });
+phone.addEventListener("input", (e) => {
+  if (e.target.value.length < 2) {
+    e.target.value = "+7";
+    return;
+  }
+  let cleanNumber = e.target.value.slice(2).replace(/\D/g, "");
+  let match = cleanNumber.match(/(\d{0,3})(\d{0,3})(\d{0,2})(\d{0,2})/);
 
+  let first = match[1];
+  let second = match[2];
+  let third = match[3];
+  let fourth = match[4];
+
+  let res = "+7";
+  if (!!first) {
+    res = `${res} (${first}`;
+  }
+  if (!!second) {
+    res = `${res}) ${second}`;
+  }
+  if (!!third) {
+    res = `${res}-${third}`;
+  }
+  if (!!fourth) {
+    res = `${res}-${fourth}`;
+  }
+  e.target.value = res;
+});
+function sendRequest(method, url, body) {
+  const headers = {
+    "Content-Type": "application/json",
+  };
+
+  return fetch(url, {
+    method: method,
+    body: JSON.stringify(body),
+    headers: headers,
+  }).then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+    return response.json().then((error) => {
+      const e = new Error("Что-то пошло не так");
+      e.data = error;
+      throw e;
+    });
+  });
+}
+
+function validateInput(form) {
+  let formReq = document.querySelectorAll(".requiredElem");
+  let errorLabel = document.querySelectorAll(".errorLabel");
+
+  errorLabel.forEach((errLabel) => {
+    addClassErrorLabel(errLabel);
+  });
+
+  formReq.forEach((element) => {
+    addClassError(element);
+  });
+
+  let emailLabel = errorLabel[1];
+  let phoneLabel = errorLabel[2];
+  let email = formReq[1];
+  let phone = formReq[2];
+  let passes = 0;
+
+  for (let id = 0; id < formReq.length; id++) {
+    const element = formReq[id];
+
+    if (element.id === "email" && element.value !== "") {
+      if (emailValidate(element)) {
+        removeClassError(element);
+        removeClassErrorLabel(errorLabel[id]);
+        passes++;
+
+        if (phoneValidate(phone)) {
+          removeClassError(phone);
+          removeClassErrorLabel(phoneLabel);
+        } else if (phone.value === "" || phone.value === "+7") {
+          removeClassError(phone);
+          removeClassErrorLabel(phoneLabel);
+          passes++;
+        }
+      }
+    } else if (element.id === "phone" && element.value !== "") {
+      if (phoneValidate(element)) {
+        removeClassError(element);
+        removeClassErrorLabel(errorLabel[id]);
+        passes++;
+        if (emailValidate(email)) {
+          removeClassErrorLabel(emailLabel);
+          removeClassError(email);
+        } else if (email.value === "") {
+          removeClassError(email);
+          removeClassErrorLabel(emailLabel);
+          passes++;
+        }
+      }
+    } else if (element.value !== "") {
+      removeClassError(element);
+      removeClassErrorLabel(errorLabel[id]);
+      passes++;
+    }
+  }
+
+  return passes;
+}
+
+function addClassErrorLabel(errLabel) {
+  errLabel.classList.add("errorLabelClass");
+}
+function removeClassErrorLabel(errLabel) {
+  errLabel.classList.remove("errorLabelClass");
+}
 function addClassError(elements) {
   elements.classList.add("errorClass");
 }
+
 function removeClassError(elements) {
   elements.classList.remove("errorClass");
 }
+
 function emailValidate(email) {
   return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(email.value);
 }
+
 function phoneValidate(phone) {
-  return /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/.test(
-    phone.value
-  );
+  return /\+7 \(\d{3}\) \d{3}-\d{2}-\d{2}/.test(phone.value);
 }
